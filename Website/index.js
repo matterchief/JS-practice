53f88636da2b48f7a996cfda2a4abe09ce1ae52b
shafee_azeem <shafeeazeemag@gmail.com>
2024-09-10 13:53:46 +0530

    sesh 3

--- Website/index.js
+++ Website/index.js
@@ -10,3 +10,31 @@ let username = "Shafee";
 let letters = [...username].join("-");
 
 console.log(letters);
+
+let vegetables = ["carrot", "potato", "capsicum"];
+let fruits = ["banana", "apples", "Orange"];
+
+let newveggies = [...vegetables];
+
+let foods = [...fruits, ...vegetables, 90, "milk"];
+console.log(foods);
+
+function openFridge(...foods) {
+  console.log(...foods);
+}
+
+function getFood(...foods) {
+  return foods;
+}
+
+const food1 = "Pizza";
+const food2 = "Hamburger";
+const food3 = "Burrito";
+const food4 = "hotdog";
+const food5 = "Ramen";
+
+//invoke function
+openFridge(food1, food2, food3, food4, food5);
+
+const edibles = getFood(food1, food2, food3, food4, food5);
+console.log(edibles);
